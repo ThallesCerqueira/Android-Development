0dd1e3e2ed754c27967b2c87cbe8bc8fb0fee8f8
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-07-18 14:02:27 -0300

    Modificando classe Pessoa e MainActivity. Modificando id's

--- AppListaCurso/app/src/main/java/devandroid/thalles/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/thalles/applistacurso/model/Pessoa.java
@@ -2,10 +2,54 @@ package devandroid.thalles.applistacurso.model;
 
 public class Pessoa {
 
-    
+    private String nome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
 
     public Pessoa() {
 
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "Nome = '" + nome + '\'' +
+                ", Sobrenome = '" + sobrenome + '\'' +
+                ", Curso Desejado = '" + cursoDesejado + '\'' +
+                ", Contato = '" + telefoneContato + '\'' +
+                '}';
+    }
 }
--- AppListaCurso/app/src/main/java/devandroid/thalles/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/thalles/applistacurso/view/MainActivity.java
@@ -10,6 +10,7 @@ import devandroid.thalles.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    String dadosPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        pessoa.setNome( "Thalles" );
+        pessoa.setSobrenome( "Cerqueira" );
+        pessoa.setCursoDesejado( "Android Development" );
+        pessoa.setTelefoneContato( "73991556511" );
+
+        dadosPessoa = "Nome: " + pessoa.getNome();
+        dadosPessoa += ", Sobrenome: " + pessoa.getSobrenome();
+        dadosPessoa += ", Curso desejado: " + pessoa.getCursoDesejado();
+        dadosPessoa += ", Telefone: " + pessoa.getTelefoneContato();
+
     }
 }
