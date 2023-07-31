fe85841397efa4e037fe04c74740de9d988460b2
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-07-31 10:13:15 -0300

    adicionando método de click - Gasto Viagem

--- GastoViagem/app/src/main/java/devandroid/thalles/gastoviagem/MainActivity.kt
+++ GastoViagem/app/src/main/java/devandroid/thalles/gastoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package devandroid.thalles.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import devandroid.thalles.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,8 +15,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener(this)
 
+    }
 
+    override fun onClick(view: View) {
+
+        if( view.id == R.id.button_calculate ) {
+            calculate()
+        }
+
+    }
+
+    private fun calculate() {
+
+        Toast.makeText( this, "Fui clicado", Toast.LENGTH_SHORT ).show()
 
     }
 }
