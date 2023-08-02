4b600fffdbc44b9200d9b408bf42f894ef0802d7
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-08-02 14:29:52 -0300

    estilizando layout

--- appMotivation/app/src/main/java/devandroid/thalles/appmotivation/MainActivity.kt
+++ appMotivation/app/src/main/java/devandroid/thalles/appmotivation/MainActivity.kt
@@ -2,10 +2,33 @@ package devandroid.thalles.appmotivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import devandroid.thalles.appmotivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView(binding.root)
+
+        // Esconde a barra de navegação
+        supportActionBar?.hide()
+
+        // Eventos de navegação
+        binding.buttonNewPhrase.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+
+        if(view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
+
+    }
+
+
 }
